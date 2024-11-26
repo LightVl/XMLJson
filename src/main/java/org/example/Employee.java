17e0d422c5371e0af9e3fd42fed9017825d97aa6
LightVl <mihail.rataev@gmail.com>
2024-11-26 19:50:04 +0300

    XmlJson examples

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int age;
+}
